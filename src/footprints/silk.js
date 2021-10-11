a7bc72c9d7dc5a5e322daa45f7160586184d2b64
Karl Grasegger <karl.grasegger@gebruederheitz.de>
2021-10-11 16:56:28 +0000

    Fixed missing param before path

--- src/footprints/silk.js
+++ src/footprints/silk.js
@@ -6,7 +6,7 @@ module.exports = {
         path: 'logo.kicad_mod',
     },
     body: p => {
-     return fs.readFileSync( p.path, 'utf8').replace(/.*\(descr.*svg2mod.*\)/,  p.at);
+     return fs.readFileSync( p.param.path, 'utf8').replace(/.*\(descr.*svg2mod.*\)/,  p.at);
     }
     
 }

686f6a37b80154ef5ddd4192363ec7fa0055734a
Karl Grasegger <karl.grasegger@gebruederheitz.de>
2021-10-16 10:37:13 +0200

    Added quick release to pucks

--- src/footprints/puck.js
+++ src/footprints/puck.js
@@ -17,6 +17,8 @@ module.exports = {
 
   (pad 1 thru_hole circle (at 0 -19.05) (size 4.4 4.4) (drill 2.2) (layers *.Cu *.Mask))
   (pad 1 thru_hole circle (at -19.05 0) (size 4.4 4.4) (drill 2.2) (layers *.Cu *.Mask)) 
+  (pad 2 thru_hole circle (at -17.05 -2 0) (size 6 6) (drill 5) (layers *.Cu *.Mask))
+  (pad 2 thru_hole circle (at 2 -21.05 0) (size 6 6) (drill 5) (layers *.Cu *.Mask)
         )
     
     `
